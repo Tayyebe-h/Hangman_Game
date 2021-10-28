be27878c37ed16747ee4109fe1c5a028e038a9d2
Tayyebe <t.hoseini1796@gmail.com>
2021-10-28 23:10:30 +0200

    Added hangman display stages

--- Hangman_Game/Hangman_Game.py
+++ Hangman_Game/Hangman_Game.py
@@ -1,28 +1,98 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+end_of_game = False
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
+
 display = []
 for _ in range(word_length):
     display += "_"
 
 
-end_of_game = False
 
 while not end_of_game:
-
  guess = input("Guess a letter: ").lower()
+ exists = False
 
  for position in range(word_length):
     letter = chosen_word[position]
     if letter == guess:
         display[position] = letter
+        exists = True
 
- print(display)
+ if not exists:
+      lives -= 1
+ 
+ print(f"{' '.join(display)}")
+ print(stages[lives]) 
+
  if not "_" in display:
      end_of_game = True
      print("You win.")
+ if lives == 0:
+      end_of_game = True
+      print("You lose.")
 

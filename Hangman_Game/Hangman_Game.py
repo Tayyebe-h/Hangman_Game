cbbceca80da38592c74d4182fe4bfe55d50cfbe0
Tayyebe <t.hoseini1796@gmail.com>
2021-10-29 00:13:11 +0200

    Added some other functionality

--- Hangman_Game/Hangman_Game.py
+++ Hangman_Game/Hangman_Game.py
@@ -24,10 +24,14 @@ while not end_of_game:
  for position in range(word_length):
     letter = chosen_word[position]
     if letter == guess:
-        display[position] = letter
+        if display[position] == "_":
+             display[position] = letter
+        else:
+            print("You've already guessed "+ guess)
 
 
  if guess not in chosen_word:
+      print("You guessed "+ guess + ", that is not in the word. You lose a life.")
       lives -= 1
       if lives == 0:
         end_of_game = True
